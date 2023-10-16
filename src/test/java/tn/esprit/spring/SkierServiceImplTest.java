de5c3034ecd87a02551a11c1520a5aa9acbf064d
mahergasmi <maher.gasmi@esprit.tn>
2023-10-16 18:03:58 +0100

    Test JUnit

--- src/test/java/tn/esprit/spring/SkierServiceImplTest.java
+++ src/test/java/tn/esprit/spring/SkierServiceImplTest.java
@@ -1,2 +1,34 @@
-package tn.esprit.spring;public class SkierServiceImplTest {
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.repositories.ISkierRepository;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.class)
+public class SkierServiceImplTest {
+
+    @Autowired
+    ISkierRepository skierRepository;
+
+    @Test
+    @Order(1)
+    public void retrieveAllSkiersTest() {
+
+        Assertions.assertNotNull(skierRepository.findAll());
+    }
+
+    @Test
+    @Order(2)
+    public void addSkierTest() {
+
+        Skier skier=new Skier();
+        Assertions.assertNotNull(skierRepository.save(skier));
+    }
+
+
 }
