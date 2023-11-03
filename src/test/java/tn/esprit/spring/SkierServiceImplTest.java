bc68c71363aa7a983130e74cb2970336e05ae42a
mahergasmi <maher.gasmi@esprit.tn>
2023-11-03 19:35:21 +0100

    junit tests

--- src/test/java/tn/esprit/spring/SkierServiceImplTest.java
+++ src/test/java/tn/esprit/spring/SkierServiceImplTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.entities.Subscription;
+import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISkierRepository;
 import tn.esprit.spring.services.ISkierServices;
+import tn.esprit.spring.services.ISubscriptionServices;
+
+import java.time.LocalDate;
 
 //@SpringBootTest
 //@TestMethodOrder(MethodOrderer.class)
@@ -13,27 +18,32 @@ public class SkierServiceImplTest {
 /*
     @Autowired
     ISkierServices skierServices;
+
+    @Autowired
+    ISubscriptionServices subscriptionServices;
+
     @Test
     @Order(1)
+    public void addSkierTest() {
+        Subscription s = subscriptionServices.addSubscription(new Subscription(1L,LocalDate.of(1999,1,1),LocalDate.of(1999,1,1),1F, TypeSubscription.ANNUAL));
+        Skier skier = new Skier(Long.valueOf(1),"First name1","last name1", LocalDate.of(1999,1,1),"Tunis", s,null,null);
+        Assertions.assertNotNull(skierServices.addSkier(skier));
+    }
+    @Test
+    @Order(2)
     public void retrieveAllSkiersTest() {
-
         Assertions.assertNotNull(skierServices.retrieveAllSkiers());
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     public void retrieveSkierTest() {
-        Skier skier=new Skier();
+        Skier skier = new Skier(Long.valueOf(1),"First name1","last name1", LocalDate.of(1999,1,1),"Tunis", null,null,null);
         Assertions.assertNotNull(skierServices.retrieveSkier(skier.getNumSkier()));
     }
 
-    @Test
-    @Order(3)
-    public void updateSkierTest() {
-        Skier skier=new Skier();
-        Assertions.assertNotNull(skierServices.addSkier(skier));
-    }
-
 */
+
+
 
 }
