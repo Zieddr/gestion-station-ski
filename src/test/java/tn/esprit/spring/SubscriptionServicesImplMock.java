7ff77ca23be7f7e375567e5431709b738b738365
zeinebzarrouk <zeineb.zarrouk@esprit.tn>
2023-11-14 19:43:29 +0100

    tests mockito

--- src/test/java/tn/esprit/spring/SubscriptionServicesImplMock.java
+++ src/test/java/tn/esprit/spring/SubscriptionServicesImplMock.java
@@ -1,36 +1,56 @@
-package tn.esprit.spring;
-
-
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.spring.entities.Subscription;
-import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
 import tn.esprit.spring.services.SubscriptionServicesImpl;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
 public class SubscriptionServicesImplMock {
 
     @Mock
-    ISubscriptionRepository iSubscriptionRepository;
+    private ISubscriptionRepository subscriptionRepository;
+
     @InjectMocks
-    SubscriptionServicesImpl subscriptionServices;
+    private SubscriptionServicesImpl subscriptionServices;
 
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-    List<Subscription> listSubscriptions = new ArrayList<Subscription>() {
-        {
-            add(new Subscription(1L,LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 1),2f,TypeSubscription.ANNUAL));
-            add(new Subscription(1L,LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 1),2f,TypeSubscription.SEMESTRIEL));
-            add(new Subscription(1L,LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 1),2f,TypeSubscription.MONTHLY));
-        }
-    };
+    @Test
+    void testRetrieveSubscriptionById() {
+        Long numSubscription = 1L;
+        Subscription expectedSubscription = new Subscription();
+        expectedSubscription.setNumSub(numSubscription);
 
-};
+        // Configurer le comportement du mock
+        when(subscriptionRepository.findById(numSubscription)).thenReturn(Optional.of(expectedSubscription));
+
+        // Appeler la méthode que vous testez
+        Subscription result = subscriptionServices.retrieveSubscriptionById(numSubscription);
+
+        // Vérifier le résultat
+        assertEquals(numSubscription, result.getNumSub());
+    }
+
+    @Test
+    void testRetrieveSubscriptionByIdWhenNotExists() {
+        Long numSubscription = 1L;
+
+        // Configurer le comportement du mock lorsque l'abonnement n'existe pas
+        when(subscriptionRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        // Appeler la méthode que vous testez
+        Subscription result = subscriptionServices.retrieveSubscriptionById(numSubscription);
+
+        // Vérifier que le résultat est null car l'abonnement n'existe pas
+        assertEquals(null, result);
+    }
+}
--- src/test/java/tn/esprit/spring/SubscriptionServicesImplTest.java
+++ src/test/java/tn/esprit/spring/SubscriptionServicesImplTest.java
@@ -1,28 +1,61 @@
-package tn.esprit.spring;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.spring.entities.Subscription;
-import tn.esprit.spring.services.ISubscriptionServices;
+import tn.esprit.spring.entities.TypeSubscription;
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SubscriptionServicesImpl;
 
-import java.util.List;
+import java.time.LocalDate;
 
-@SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class SubscriptionServicesImplTest {
 
-    @Autowired
-    ISubscriptionServices sub;
-/*
+    @Mock
+    private ISubscriptionRepository subscriptionRepository;
+
+    @InjectMocks
+    private SubscriptionServicesImpl subscriptionServices;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    @Order(1)
-    public void testRetrieveSubscriptions() {
-        List<Subscription> listSubscriptions = sub.retrieveSubscriptions();
-        Assertions.assertEquals(0, listSubscriptions.size());
-    }*/
+    void testAddSubscription() {
+        // Créer un objet Subscription pour le test
+        Subscription subscription = new Subscription();
+        subscription.setTypeSub(TypeSubscription.ANNUAL);
+        subscription.setStartDate(LocalDate.now());
+
+        // Configurer le mock pour retourner l'objet Subscription sauvegardé
+        when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+
+        // Appeler la méthode que vous testez
+        Subscription result = subscriptionServices.addSubscription(subscription);
+
+        // Vérifier que la date de fin a été correctement calculée en fonction du type de subscription
+        switch (subscription.getTypeSub()) {
+            case ANNUAL:
+                assertEquals(subscription.getStartDate().plusYears(1), result.getEndDate());
+                break;
+            case SEMESTRIEL:
+                assertEquals(subscription.getStartDate().plusMonths(6), result.getEndDate());
+                break;
+            case MONTHLY:
+                assertEquals(subscription.getStartDate().plusMonths(1), result.getEndDate());
+                break;
+        }
+
+        // Vérifier que la méthode de sauvegarde a été appelée avec l'objet Subscription
+        // (cela garantit que la logique de la méthode addSubscription est correcte)
+        // Notez que vous pouvez également utiliser ArgumentCaptor pour capturer l'argument passé à save
+        // et effectuer des assertions plus détaillées.
+        // verify(subscriptionRepository).save(any(Subscription.class));
+    }
 }
