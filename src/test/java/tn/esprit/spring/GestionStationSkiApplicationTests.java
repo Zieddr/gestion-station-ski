e9a6d6e173be5f0aa76379954b78a4c70691bfc8
mahergasmi <maher.gasmi@esprit.tn>
2023-11-15 09:36:58 +0100

    jacoco dependency

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionStationSkiApplicationTests {
+public class GestionStationSkiApplicationTests {
 
 	@Test
 	void contextLoads() {
